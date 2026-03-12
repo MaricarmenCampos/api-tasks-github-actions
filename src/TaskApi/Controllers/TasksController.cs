1fa34fed0ec66ae4841c5bb25f2aa0ebd4f1fa7e
Maricarmen Campos <maricperez@uv.mx>
2026-03-12 11:45:08 -0600

    Corregido

--- src/TaskApi/Controllers/TasksController.cs
+++ src/TaskApi/Controllers/TasksController.cs
@@ -23,8 +23,8 @@ public class TasksController : ControllerBase
     public IActionResult GetById(int id)
     {
         var task = _repo.GetById(id);
-        //return task is null ? NotFound() : Ok(task);
-        return BadRequest();
+        return task is null ? NotFound() : Ok(task);
+        //return BadRequest();
     }
 
     [HttpPost]
